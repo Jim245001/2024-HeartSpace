fb36403239fe4abe22156a4c044a353dd55f8ab0
Jim245 <jim245.001@gmail.com>
2025-01-02 10:30:28 +0800

    1/2早上-1 Jim

---
+++ HeartSpace/Controllers/AdminControllerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HeartSpace.Controllers.Admin
+{
+    public class AdminControllerController : Controller
+    {
+        // GET: AdminController
+        public ActionResult ManageGroup()
+        {
+            return View();
+        }
+    }
+}
