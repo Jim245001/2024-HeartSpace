d0e4f9613f0c3b40cfb07692155561f46451049c
凱琪 陳 <kelly870315@gmail.com>
2025-01-02 15:09:31 +0800

    modify PostDetailsModel, add comments

--- HeartSpace/Models/AppDbContext/PostDetailsModel.cs
+++ HeartSpace/Models/AppDbContext/PostDetailsModel.cs
@@ -6,21 +6,23 @@ namespace YourNamespace.Models
 {
     public class PostDetailsModel
     {
-        [Key] // 主鍵
+        [ [Key]
         public int Id { get; set; }
+
+        public int MemberId { get; set; }
 
         [Required(ErrorMessage = "標題是必填項")]
         [StringLength(100, ErrorMessage = "標題最多 100 字")]
-        public string Title { get; set; } // 貼文標題
+        public string Title { get; set; }
 
         [Required(ErrorMessage = "內文是必填項")]
-        public string Content { get; set; } // 貼文內容
+        public string PostContent { get; set; }
 
-        public string ImagePath { get; set; } // 圖片路徑
+        public string img { get; set; }
 
-        public DateTime CreatedAt { get; set; } = DateTime.Now; // 發文時間
+        public DateTime PublishTime { get; set; } = DateTime.Now;
 
-        public string Author { get; set; } // 發文人名稱
+        public int CategoryId { get; set; }
 
         public virtual ICollection<CommentModel> Comments { get; set; } // 留言集合
     }
--- HeartSpace/Models/PostViewModel.cs
+++ HeartSpace/Models/PostViewModel.cs
@@ -7,11 +7,10 @@ namespace HeartSpace.Models
 {
 	public class PostViewModel
 	{
-		public string CoverImage { get; set; }  // 貼文封面圖
-		public string AuthorImage { get; set; } // 原PO頭像
-		public string AuthorId { get; set; }    // 原PO ID
+		public string PostImg { get; set; }  // 貼文封面圖
+		public string MemberImg { get; set; } // 原PO頭像
+		public string MemberId { get; set; }    // 原PO ID
 		public string Description { get; set; } // 貼文描述
-		public List<string> Tags { get; set; }  // 標籤列表
 		public string Timestamp { get; set; }   // 發佈時間
 	}
 
