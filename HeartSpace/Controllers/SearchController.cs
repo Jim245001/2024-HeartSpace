fe3d55ae785e0ee41deb5dd39ae8d789715870de
Jim245 <jim245.001@gmail.com>
2025-01-02 13:02:46 +0800

    搞定搜尋貼文草稿

--- HeartSpace/Controllers/SearchController.cs
+++ HeartSpace/Controllers/SearchController.cs
@@ -1,17 +1,29 @@
-﻿using System;
+﻿using HeartSpace.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Collections.Generic;
+using System;
+
 
 namespace HeartSpace.Controllers
 {
-    public class SearchController : Controller
-    {
-        // GET: Search
-        public ActionResult SearchEvent()
-        {
-            return View();
-        }
-    }
+	public class SearchController : Controller
+	{
+		// GET: Search
+		public ActionResult SearchEvent()
+		{
+			return View();
+		}
+
+		public ActionResult SearchPost()
+		{
+			return View();
+		}
+		
+
+
+	}
 }
